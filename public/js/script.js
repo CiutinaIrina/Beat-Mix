730d048e650222bff89f167a3b80e41c0cb5420a
Ciutina Irina <aiciutina@gmail.com>
2021-06-17 21:03:31 +0300

    Finished Task 1 + package-lock.json added

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,26 @@
-// Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+//Task 0 - helpful functions
+
+const ok = (text = 'Ok!') => {
+    console.log(text);
+}
+
+//Task 1 - the arrays
+
+var kicks = [];
+var snares = [];
+var hiHats = [];
+var rideCymbals = [];
+
+var arrayList = [kicks, snares, hiHats, rideCymbals];
+
+ok(arrayList);
+
+for(i=0;i<4;i++){
+    for(j=0;j<16;j++){
+        arrayList[i].push(false);
+    }
+}
+
+ok(arrayList);
+
 
